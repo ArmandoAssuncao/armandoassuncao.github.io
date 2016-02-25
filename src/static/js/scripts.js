f5a5c5f11ea3111fd9a1d46e839d348b0b2afc39
armandoassuncao <armando.assuncao.93@gmail.com>
2016-02-24 23:08:15 -0300

    Update Footer

--- src/static/js/scripts.js
+++ src/static/js/scripts.js
@@ -8,17 +8,17 @@ $(document).ready(function(){
 function showHideContactForm(){
 	var element = this;
 	function show(){
-		$('footer').css('position', 'relative');
-		$('footer').animate({bottom: $('#contact_form').css('height')}, 700, 'linear');
-		$('#contact_form').css('position', 'absolute');
+		$('footer').css('position', 'relative').fadeTo(0, 0.96);
+		$('footer').animate({bottom: $('#container_contact_form').css('height')}, 700, 'linear');
+		$('#container_contact_form').css('position', 'absolute')
 
 		$(element).one('click', hide);
 	}
 
 	function hide(){
 		$('footer').animate({bottom: '0'}, 700, 'linear', function(){
-			$('#contact_form').css('position', 'fixed')
-		});
+			$('#container_contact_form').css('position', 'fixed')
+		}).fadeTo("fast", 1);
 
 		$(element).one('click', show);
 	}

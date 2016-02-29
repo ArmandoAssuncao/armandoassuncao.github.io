1b246377d5cea83ae1b17d0c917fad254d890188
armandoassuncao <armando.assuncao.93@gmail.com>
2016-02-29 03:11:25 -0300

    add darkenPage

--- src/static/js/scripts.js
+++ src/static/js/scripts.js
@@ -10,7 +10,8 @@ function showHideContactForm(){
 	function show(){
 		$('footer').css('position', 'relative').fadeTo(0, 0.8);
 		$('footer').animate({bottom: $('#container_contact_form').css('height')}, 700, 'linear');
-		$('#container_contact_form').css('position', 'absolute')
+		$('#container_contact_form').css('position', 'absolute');
+		darkenPage(true);
 
 		$(element).one('click', hide);
 	}
@@ -18,10 +19,31 @@ function showHideContactForm(){
 	function hide(){
 		$('footer').animate({bottom: '0'}, 700, 'linear', function(){
 			$('#container_contact_form').css('position', 'fixed')
-		}).fadeTo("fast", 1);
+		}).fadeTo('fast', 1);
+		darkenPage(false);
 
 		$(element).one('click', show);
 	}
 
    show();
+}
+
+// Helpers ////////
+
+function darkenPage(show){
+	if(show){
+		$('body').append($('<div>').css({
+			position: 'fixed'
+			,width: '100%'
+			,height: '100%'
+			,'background-color': '#000'
+			,opacity: 0.6
+			,'z-index': 999
+			,top: 0
+			,left: 0
+		}).attr('id','darken_page').fadeTo(500, 0.6));
+	}
+	else{
+		$('#darken_page').fadeTo(500, 0, function(){this.remove()});
+	}
 }
